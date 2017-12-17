f9a607a6f4430ae81c762637ef66bcff3dc67bf0
Abhishek Sharma <hello.abhishek@outlook.com>
2017-12-18 01:35:26 +0530

    leaderboard add

--- platforms/android/assets/www/js/home.js
+++ platforms/android/assets/www/js/home.js
@@ -1,27 +1,34 @@
-document.addEventListener('deviceready', function(){
-StatusBar.backgroundColorByHexString('#28C1F9');});
+document.addEventListener('deviceready', function () {
+    StatusBar.backgroundColorByHexString('#28C1F9');
+});
+
+var currentCard = 1;
+var bars = document.getElementById('bars').childNodes;
 
 function onmousedown(event) {
-		console.log(event);
-	var pageX = (event.type.toLowerCase() === 'mousedown') ? event.pageX : event.touches[0].pageX;
-		dragging = {
-			startedAt: {
-				x: pageX,
-				y: event.clientY
-			},
-			lastDiff: {x:0,y:0},
-			card: this
-		};
-	};
+    console.log(event);
+    var pageX = (event.type.toLowerCase() === 'mousedown') ? event.pageX : event.touches[0].pageX;
+    dragging = {
+        startedAt: {
+            x: pageX,
+            y: event.clientY
+        },
+        lastDiff: {
+            x: 0,
+            y: 0
+        },
+        card: this
+    };
+};
 
 
-Element.prototype.remove = function() {
+Element.prototype.remove = function () {
     this.parentElement.removeChild(this);
 }
 
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function () {
+    for (var i = this.length - 1; i >= 0; i--) {
+        if (this[i] && this[i].parentElement) {
             this[i].parentElement.removeChild(this[i]);
         }
     }
@@ -30,64 +37,119 @@ NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
 var dragging = null;
 
 function initCards() {
-	var cards = document.getElementsByTagName("card");
-	for(var i=0; i<cards.length; i++) {
-		var card = cards[i];
-		card.onmousedown = onmousedown;
-		card.ontouchstart = onmousedown;
-	}
-	
-	document.body.onmousemove = function(event){
-			if(!dragging)
-				return;
-		var pageX = (event.type.toLowerCase() === 'mousemove') ? event.pageX : event.touches[0].pageX;
-			var diff = {
-				x: pageX - dragging.startedAt.x,
-				y: event.clientY - dragging.startedAt.y
-			};
-		dragging.lastDiff = diff;
-			dragging.card.style.webkitTransform = "translateX("+diff.x+"px) rotate("+(diff.x/30)+"deg)";
-		dragging.card.style.opacity = 1-Math.min(1,Math.abs(0.002*diff.x));
-		};
-	document.body.ontouchmove = document.body.onmousemove;
-    document.body.ontouchend = function(event){
-		if(!dragging)
-				return;
-		var toBeDeleted = Math.abs(dragging.lastDiff.x) > dragging.card.offsetWidth / 2;
-		if(!toBeDeleted) {
-				dragging.card.style.webkitTransform = "translateX(0) rotate(0)";
-				dragging.card.style.opacity = 1;
-				dragging = null;
-			} else {
-				dragging.card.style.webkitTransform = "translateX("+(dragging.lastDiff.x>0 ? 1000 : -1000)+"px) rotate("+(dragging.lastDiff.x>0 ? 90 : -90)+"deg)";
-				document.body.ontouchend = document.body.onmouseup;
-				console.log(dragging.card.style.webkitTransform);
-				dragging.card.style.opacity = 0;
-				setTimeout(function(){
-					dragging.card.remove();
-				}, 200);
-			}
-		}; 
-	document.body.onmouseup = function(event){
-		if(!dragging)
-				return;
-		var toBeDeleted = Math.abs(dragging.lastDiff.x) > dragging.card.offsetWidth / 2;
-		if(!toBeDeleted) {
-				dragging.card.style.webkitTransform = "translateX(0) rotate(0)";
-				dragging.card.style.opacity = 1;
-				dragging = null;
-			} else {
-				dragging.card.style.webkitTransform = "translateX("+(dragging.lastDiff.x>0 ? 1000 : -1000)+"px) rotate("+(dragging.lastDiff.x>0 ? 90 : -90)+"deg)";
-				document.body.ontouchend = document.body.onmouseup;
-				console.log(dragging.card.style.webkitTransform);
-				dragging.card.style.opacity = 0;
-				setTimeout(function(){
-					dragging.card.remove();
-				}, 200);
-			}
-		}; 
-};
+    var cards = document.getElementsByTagName("card");
+    for (var i = 0; i < cards.length; i++) {
+        var card = cards[i];
+        card.onmousedown = onmousedown;
+        card.ontouchstart = onmousedown;
+    }
 
-window.onload = function() {
-  initCards();
+    document.body.onmousemove = function (event) {
+        if (!dragging)
+            return;
+        var pageX = (event.type.toLowerCase() === 'mousemove') ? event.pageX : event.touches[0].pageX;
+        var diff = {
+            x: pageX - dragging.startedAt.x,
+            y: event.clientY - dragging.startedAt.y
+        };
+        dragging.lastDiff = diff;
+        dragging.card.style.webkitTransform = "translateX(" + diff.x + "px) rotate(" + (diff.x / 30) + "deg)";
+        dragging.card.style.opacity = 1 - Math.min(1, Math.abs(0.002 * diff.x));
+    };
+    document.body.ontouchmove = document.body.onmousemove;
+    document.body.ontouchend = function (event) {
+        if (!dragging)
+            return;
+        var toBeDeleted = Math.abs(dragging.lastDiff.x) > dragging.card.offsetWidth / 2;
+        if (!toBeDeleted) {
+            dragging.card.style.webkitTransform = "translateX(0) rotate(0)";
+            dragging.card.style.opacity = 1;
+            dragging = null;
+            console.log('not to be deleted');
+        } else {
+            dragging.card.style.webkitTransform = "translateX(" + (dragging.lastDiff.x > 0 ? 1000 : -1000) + "px) rotate(" + (dragging.lastDiff.x > 0 ? 90 : -90) + "deg)";
+            document.body.ontouchend = document.body.onmouseup;
+            console.log(dragging.card.style.webkitTransform);
+            dragging.card.style.opacity = 0;
+            setTimeout(function () {
+                dragging.card.remove();
+            }, 200);
+            if (currentCard === 7) {
+                console.log('i was called');
+                $('#end').css('opacity', 1);
+            }
+            bars[currentCard].classList.remove('current');
+            currentCard = currentCard + 2;
+            bars[currentCard].classList.add('current');
+
+        }
+    };
+    document.body.onmouseup = function (event) {
+        if (!dragging)
+            return;
+        var toBeDeleted = Math.abs(dragging.lastDiff.x) > dragging.card.offsetWidth / 2;
+        if (!toBeDeleted) {
+            dragging.card.style.webkitTransform = "translateX(0) rotate(0)";
+            dragging.card.style.opacity = 1;
+            dragging = null;
+        } else {
+            dragging.card.style.webkitTransform = "translateX(" + (dragging.lastDiff.x > 0 ? 1000 : -1000) + "px) rotate(" + (dragging.lastDiff.x > 0 ? 90 : -90) + "deg)";
+            document.body.ontouchend = document.body.onmouseup;
+            console.log(dragging.card.style.webkitTransform);
+            dragging.card.style.opacity = 0;
+            setTimeout(function () {
+                dragging.card.remove();
+            }, 200);
+            if (currentCard === 7) {
+                console.log('i was called');
+                $('#end').css('opacity', 1);
+            }
+            bars[currentCard].classList.remove('current');
+            currentCard = currentCard + 2;
+            bars[currentCard].classList.add('current');
+
+        }
+    };
+};
+window.onload = function () {
+    initCards();
 }
+
+$('#testvariables').hammer().on('tap', function () {
+    setTimeout(function () {
+        $('.bars').remove();
+        document.getElementById('test').style.display = "block";
+        document.getElementById('test').style.opacity = 1;
+        $('#pane4').css('opacity', 0);
+        $('#pane4').remove();
+        document.getElementById('v1').style.opacity = 1;
+    }, 200);
+    let card = (document.getElementById('testvariables').parentElement);
+});
+
+var v1answer;
+$('.option').hammer().on('tap', function (e) {
+    var wut = document.getElementsByClassName('option');
+    let i;
+    for (i = 0; i < wut.length; i++) {
+        wut[i].classList.remove('got');
+    }
+    e.target.classList.add('got');
+    v1answer = e.target.innerHTML;
+});
+
+$('#ansv1').hammer().on('tap', function (e) {
+    $('#v1').css('opacity', 0);
+    setTimeout(function () {
+        $('#v1').remove();
+        if (v1answer === 'B. 12,457') {
+            $('.galat').remove();
+            $('.cards').css('background-color', '#64C56E');
+            $('.sahi').css('opacity', 1);
+        } else {
+            $('.sahi').remove();
+            $('.cards').css('background-color', '#ED4C4C');
+            $('.galat').css('opacity', 1);
+        }
+    }, 300);
+});
